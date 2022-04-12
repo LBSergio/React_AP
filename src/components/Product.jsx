9e07e6d6c3fa2604091ef162dcc4c6142194d170
LBSergio <1999lopezbautistasergio@gmail.com>
2022-04-12 05:34:09 +0200

    Add checkout

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,9 +5,15 @@ const Product = ({ product }) => {
     <div className='Products-item'>
         <img src={ product.image } alt={product.title} />
         <div className='Product-item-info'>
-            <h2>{ product.title }</h2>
-            <span>${ product.price }</span>
-            <p>{ product.description }</p>
+            <h2>
+                { product.title }
+            </h2>
+            <span>
+                ${ product.price }
+            </span>
+            <p>
+                { product.description }
+            </p>
         </div>
         <button type='button'>
             Buy
--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -3,7 +3,32 @@ import '@Scomponents/Checkout.css';
 
 const Checkout = () => {
     return(
-        <h1>Checkout</h1>
+        <div className='Checkout'>
+            <div className='Checkout-content'>
+                <h3>Order list</h3>
+                <div className='Checkout-item'>
+                    <div className='Checkout-element'>
+                        <h4>
+
+                        </h4>
+                        <span>
+
+                        </span>
+                    </div>
+                    <button type='button'>
+                        Delete element
+                    </button>
+                </div>
+            </div>
+            <div className='Checkout-sidebar'>
+                <h3>
+                    Total price: $
+                </h3>
+                <button type='button'>
+                    Continue with the order
+                </button>
+            </div>
+        </div>
     );
 }
 
