4eea0f0e5a6aa68fffea7f1a19339cf07bd92e21
LBSergio <1999lopezbautistasergio@gmail.com>
2022-04-12 05:45:33 +0200

    Add information

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -3,7 +3,51 @@ import '@Scomponents/Information.css';
 
 const Information = () => {
     return(
-        <h1>Information</h1>
+        <div className='Information'>
+            <div className='Information-content'>
+                <div className='Information-head'>
+                    <h2>
+                        Information of contact
+                    </h2>
+                </div>
+                <div className='Information-form'>
+                    <form action=''>
+                        <input type='text' placeholder='Full name' name='name'/>
+                        <input type='text' placeholder='Email' name='email'/>
+                        <input type='text' placeholder='Address' name='address'/>
+                        <input type='text' placeholder='Apto' name='apto'/>
+                        <input type='text' placeholder='City' name='city'/>
+                        <input type='text' placeholder='Country' name='country'/>
+                        <input type='text' placeholder='State' name='state'/>
+                        <input type='text' placeholder='CP' name='cp'/>
+                        <input type='text' placeholder='Phone' name='phone'/>
+                    </form>
+                </div>
+                <div className='Information-buttons'>
+                    <div className='Information-back'>
+                        Back
+                    </div>
+                    <div className='Information-next'>
+                        Pay
+                    </div>
+                </div> 
+            </div>
+            <div className='Information-sidebar'>
+                <h3>
+                    Order
+                </h3>
+                <div className='Information-item'>
+                    <div className='Information-element'>
+                        <h4>
+
+                        </h4>
+                        <span>
+                            
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
