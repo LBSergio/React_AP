22513fdd758a71bf94317aec2426f5a3cd3aa5f1
LBSergio <1999lopezbautistasergio@gmail.com>
2022-04-21 13:42:02 +0200

    App context in products

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AppContext } from '@context/AppContext';
 import '@Scomponents/Header.css';
 
 export const Header = () => {
+  const { state } = React.useContext( AppContext );
+  const { cart } = state;
+
   return (
     <div className='Header'>
         <h1 className='Header-title'>
@@ -14,6 +18,7 @@ export const Header = () => {
           <Link to='/checkout'>
             <i className='fas fa-shopping-basket fa-2x'/>
           </Link>
+          {cart.length > 0  && <div className='Header-alert'> {cart.length} </div>}
         </div>
     </div>
   )
